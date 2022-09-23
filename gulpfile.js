8122a05f60433e7fe600bcd69095c7cef7cb46cc
Komarovskiy <komarovskiy9099@gmail.com>
2022-09-23 21:50:03 +0500

    Send

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('prefixer', function(done) {
+gulp.task('prefixer', function() {
     gulp.src('*.css')
     .pipe(autoprefixer({
         cascade: false
