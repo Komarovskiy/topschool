327f2b39a3d595e2cc323ff8c101571c18d69528
Komarovskiy <komarovskiy9099@gmail.com>
2022-09-30 01:33:57 +0500

    City window

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
 
+    $('header__btn').on('click', function () {
+        $('.dropdown').removeClass('dropdown--close');
+    });
+    $('dropdown__close').on('click', function () {
+        $('.dropdown').addClass('dropdown--close');
+    });
+    
 })
